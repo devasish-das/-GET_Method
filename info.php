9c9e890450368ac78e599379a029ea3cb821ec4f
devasish-das <devesish@gmail.com>
2024-06-02 16:34:27 +0600

    learn get method in php

---
+++ info.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+if(isset($_GET["save"]))
+{
+    $a= $_GET["name"];
+    $b= $_GET["uni"];
+
+
+
+
+    echo "<h1>Bruh</h1>";
+    echo "Your Name is $a";
+    echo "<br>";
+    echo "And your universiyt is $b";
+    echo "<br>";
+
+}
+
+
+
+?>
